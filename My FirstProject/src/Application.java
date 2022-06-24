e4ce9b7c7868957e858f7b052ced339ad51f840c
Jg2012 <jg764609@southeast.edu>
2022-06-24 10:53:13 -0500

    Second attempt

---
+++ My FirstProject/src/Application.java
@@ -0,0 +1,39 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * primititve:
+		 * int - an integer
+		 * double - decimal places 32.89235
+		 * float - precieions decimal data type 45.3987
+		 * long - like an int, numbers can be higher
+		 * short - like an int, less of number range
+		 * byte - 8 bits of data 
+		 * char - a single character a, B A & 
+		 * boolean - true or false 
+		 * 
+		 *Objects 
+		 *String - is textual data a string of characters
+		 */
+		
+		//variable declaration - type, identifier, assignment, operation, value semicolon
+		int age = 67; 
+		double accountBalance = 34.67;
+		char middleinitial = 'C';
+		boolean ishotOutside = true; 
+		String firstName = "Jose";
+		
+		System.out.println(age);
+		System.out.println(accountBalance);
+		System.out.println(middleinitial);
+		System.out.println(ishotOutside);
+		System.out.println(firstName);
+		
+		
+	
+		
+	}
+
+}  
---
+++ Operations/src/Operation.java
@@ -0,0 +1,25 @@
+
+public class Operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Operator performs actions on operands 
+		// + - " / 
+		int age = 34 * 2; 
+		System.out.println(age);
+		boolean isAge30 = age  == 30; 
+		System.out.println(isAge30);
+		
+		System.out.println(age == 68);
+		
+		age = age + 1;
+		System.out.println(age);
+		
+		String firstName = "Tommy";
+		String lastName = "Smith";
+		String fullName = firstName + " " + lastName; //concatenation 
+		System.out.println(fullName);
+
+	}
+
+}
